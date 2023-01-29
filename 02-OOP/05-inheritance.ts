13887a7320b8b1a8c0fe6648480039e6b43e0022
LEESEUNGJAE <karmaup0711@gmail.com>
2023-01-29 20:11:29 +0900

    inheritance

---
+++ 02-OOP/05-inheritance.ts
@@ -0,0 +1,84 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAM_PER_SHOT = 7; // class level : static을 사용하면 인스턴스가 생성될 때마다 중복으로 데이터가 생성되는 것을 방지할 수 있다.
+    private coffeeBeans: number = 0; // instance (object) level
+
+    public constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error('value for beans should be greater than 0');
+      }
+      this.coffeeBeans += beans;
+      console.log('filled~!');
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log('heating up ~ 🔥');
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots...☕️`);
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+
+    clean(): void {
+      console.log('cleaning the machine');
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(beans: number, public readonly serialNumber: string) {
+      super(beans);
+    }
+    private steamMilk(): void {
+      console.log('Steaming some milk... 🍼');
+    }
+    makeCoffee(shots: number): { shots: number; hasMilk: boolean } {
+      const coffee = super.makeCoffee(shots);
+      this.steamMilk();
+      return {
+        ...coffee,
+        hasMilk: true,
+      };
+    }
+  }
+
+  const machine = new CoffeeMachine(77);
+  const latteMachine = new CaffeLatteMachine(77, 'SERIAL');
+  const coffee = latteMachine.makeCoffee(1);
+  console.log(coffee);
+  console.log(latteMachine.serialNumber);
+}
