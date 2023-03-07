02427c79f4cf5dd3939cd735e4076d829f4ef91a
LEESEUNGJAE <karmaup0711@gmail.com>
2023-03-07 21:52:03 +0900

    debug

--- 07-config/dist/main.js
+++ 07-config/dist/main.js
@@ -1,2 +1,13 @@
-"use strict";
-console.log('hihi');
+'use strict';
+class Car {
+    constructor() {
+        this.engine = 0;
+    }
+    move() {
+        const engine = this.engine + 1;
+        console.log(engine);
+    }
+}
+const car = new Car();
+car.move();
+//# sourceMappingURL=main.js.map
